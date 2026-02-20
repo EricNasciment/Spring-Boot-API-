e4e6ba38f54e3f0911d46b9f6eb61641046661c2
Eric Silva <ericsilva075@gmail.com>
2026-02-20 20:40:25 -0300

    Add Lombok

--- src/main/java/com/spring/api/simpleproject/models/Task.java
+++ src/main/java/com/spring/api/simpleproject/models/Task.java
@@ -12,9 +12,19 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter 
+@Setter
+@EqualsAndHashCode
 public class Task {
 
     public static final String TABLE_NAME = "task";
@@ -33,62 +43,5 @@ public class Task {
     @Size(min = 1, max = 255)
     private String description;
 
-    public Task(){}
-
-    public Task(Long id, User user, String description){
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public User getUser(){
-        return user;
-    }
-    
-    public void setUser(User user){
-        this.user = user;
-    }
-
-    public String getDescription(){
-        return description;
-    }
-
-    public void setDescription(String description){
-        this.description = description;
-    }
-
-    @Override   
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (! (obj instanceof Task))
-            return false;
-        Task other = (Task) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) 
-               && Objects.equals(this.description, other.description);
-    }
 
 }
--- src/main/java/com/spring/api/simpleproject/models/User.java
+++ src/main/java/com/spring/api/simpleproject/models/User.java
@@ -9,17 +9,27 @@ import jakarta.persistence.GenerationType;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Table (name = User.Table_Name)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter 
+@Setter
+@EqualsAndHashCode
 public class User {
 
     public interface CreateUser{}
@@ -44,78 +54,11 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user") //um para muitos com a entidade task!)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //para não retornar as tarefas na resposta da api!
     private List<Task> tasks = new ArrayList<Task>();
 
 
-    public User(){}
-
-    public User( Long id,String username,String password){
-         this.id = id;
-         this.userName = username;
-         this.password = password;
-    }
-
-
-
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setuserName(String username){
-        this.userName = username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    @Override   
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (! (obj instanceof User))
-            return false;
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.userName, other.userName) 
-               && Objects.equals(this.password, other.password);
-    }
+   
 
 
 }
